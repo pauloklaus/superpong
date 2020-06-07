f3fa0f12af2eb585c016035618b338445d95fbe0
Paulo Sergio Klaus <ti@pauloklaus.com.br>
2020-06-07 15:42:16 -0300

    Espaço para placar. Constantes para dimensão da bolinha e jogadores. Iniciar movimento da bolinha quando um jogador se mover. Método para testar toque da bola no jogador. Método para testar se bola ultrapassou jogador.

--- public/pong.js
+++ public/pong.js
@@ -8,7 +8,7 @@ const movimento = {
     abaixo: 1
 };
 const campo = {
-    margemSuperior: 10,
+    margemSuperior: 50,
     margemInferior: canvas.height - 10,
     margemEsquerda: 10,
     margemDireita: canvas.width - 10
@@ -18,29 +18,50 @@ const objetosDaTela = {
     jogador2: null,
     bolinha: null
 };
+const dimensaoBolinha = {
+    altura: 30,
+    largura: 30
+};
+const dimensaoJogador = {
+    altura: 120,
+    largura: 30
+};
 
-function mapearTeclasMovimento(jogador, teclaAcima, teclaAbaixo) {
+function gerarMovimentoEntre(min, max) {
+    return Math.random() * (max - min) + min;
+};
+
+function mapearTeclasMovimento(bolinha, jogador, teclaAcima, teclaAbaixo) {
     window.addEventListener('keydown', function(event) {
-        if (event.key == teclaAcima)
-            jogador.movimentar(movimento.acima);
-        else if (event.key == teclaAbaixo)
-            jogador.movimentar(movimento.abaixo);
+        if (event.key == teclaAcima) {
+            bolinha.iniciarMovimento();
+            jogador.iniciarMovimento(movimento.acima);
+        }
+        else if (event.key == teclaAbaixo) {
+            bolinha.iniciarMovimento();
+            jogador.iniciarMovimento(movimento.abaixo);
+        }
     });
 
     window.addEventListener('keyup', function(event) {
-        if (event.key == teclaAcima || event.key == teclaAbaixo)
+        if (event.key == teclaAcima || event.key == teclaAbaixo) {
+            bolinha.iniciarMovimento();
             jogador.pararMovimento();
+        }
     });
 }
 
 function criarJogador(posicao) {
+    largura = dimensaoJogador.largura;
+    altura = dimensaoJogador.altura;
+
     return {
-        largura: 30,
-        altura: 120,
-        linha: 10,
-        coluna: posicao == 'esq' ? 10 : canvas.width - 40,
+        largura: largura,
+        altura: altura,
+        linha: campo.margemSuperior + ((campo.margemInferior - campo.margemSuperior) / 2) - (altura / 2),
+        coluna: posicao == 'esq' ? 10 : campo.margemDireita - largura,
         velocidadeMovimento: 10,
-        movimentando: 0,
+        movimentando: movimento.parado,
         desenhar() {
             contexto.beginPath();
             contexto.rect(this.coluna, this.linha, this.largura, this.altura);
@@ -60,42 +81,73 @@ function criarJogador(posicao) {
             if (this.linha < campo.margemInferior - this.altura)
                 this.linha = this.linha + this.velocidadeMovimento;
         },
-        movimentar(movimentar) {
+        iniciarMovimento(movimentar) {
             this.movimentando = movimentar;
         },
         pararMovimento() {
             this.movimentando = movimento.parar;
+        },
+        defendeuBolinha(linha) {
+            return (linha >= this.linha && linha <= this.linha + this.altura);
         }
     };
 }
 
 function criarBolinha(posicao) {
-    largura = 30;
-    altura = 30;
+    largura = dimensaoBolinha.largura;
+    altura = dimensaoBolinha.altura;
 
     return {
         velocidade: 5,
         largura: largura,
         altura: altura,
-        linha: (canvas.height / 2) - (altura / 2),
-        coluna: (canvas.width / 2) - (largura / 2),
-        movimentarLinha: 1,
-        movimentarColuna: 1,
+        linha: campo.margemSuperior + ((campo.margemInferior - campo.margemSuperior) / 2) - (altura / 2),
+        coluna: campo.margemEsquerda + (campo.margemDireita / 2) - (largura / 2),
+        movimentarLinha: movimento.parado,
+        movimentarColuna: movimento.parado,
         desenhar() {
             contexto.beginPath();
             contexto.rect(this.coluna, this.linha, this.largura, this.altura);
             contexto.fillStyle = 'white';
             contexto.fill();
         },
+        iniciarMovimento() {
+            if (this.movimentarLinha == 0) {
+                this.movimentarLinha = gerarMovimentoEntre(-1,1);
+                this.movimentarColuna = gerarMovimentoEntre(-1,1);
+            }
+        },
+        pararMovimento() {
+            this.velocidade = 0;
+        },
+        mudarDirecaoLinha() {
+            this.movimentarLinha *= -1;
+        },
+        mudarDirecaoColuna() {
+            this.movimentarColuna *= -1;
+        },
         atualizar() {
+            if (this.velocidade <= 0)
+                return;
+
             if (this.linha <= campo.margemSuperior || this.linha >= (campo.margemInferior - this.altura))
-                this.movimentarLinha *= -1;
+                this.mudarDirecaoLinha();
 
             if (this.coluna <= campo.margemEsquerda || this.coluna >= campo.margemDireita - this.largura)
-                this.movimentarColuna *= -1
+                this.mudarDirecaoColuna();
 
             this.linha += (this.movimentarLinha * this.velocidade);
             this.coluna += (this.movimentarColuna * this.velocidade);
+
+            return;
+        },
+        tocouMargemEsquerda() {
+            const margemEsquerda = campo.margemEsquerda + dimensaoJogador.largura;
+            return (this.coluna <= margemEsquerda)
+        },
+        tocouMargemDireita() {
+            const margemDireita = campo.margemDireita - ( dimensaoJogador.largura * 2 );
+            return (this.coluna >= margemDireita)
         }
     };
 }
@@ -104,25 +156,57 @@ const telas = {
     inicio: {
         configurar() {
             objetosDaTela.jogador1 = criarJogador('esq');
-            mapearTeclasMovimento(objetosDaTela.jogador1, 'q', 'a')
-
             objetosDaTela.jogador2 = criarJogador('dir');
-            mapearTeclasMovimento(objetosDaTela.jogador2, '[', ']')
-            
             objetosDaTela.bolinha = criarBolinha();
+
+            mapearTeclasMovimento(objetosDaTela.bolinha, objetosDaTela.jogador1, 'q', 'a');
+            mapearTeclasMovimento(objetosDaTela.bolinha, objetosDaTela.jogador2, '[', ']');
+            contexto.font = '30px Arial';
+            contexto.fillText('qa', 10, 10);
         }
     },
     jogo: {
+        // desenharPlacar() {
+        //     contexto.setLineDash();
+        //     contexto.fillStyle = 'white';
+        //     contexto.font = '30px Arial';
+        //     contexto.text("Teste", 10, 50);
+        // },
         desenhar() {
             contexto.fillStyle = 'black';
             contexto.fillRect(0, 0, canvas.width, canvas.height);
 
+            contexto.beginPath();
+            contexto.moveTo(campo.margemEsquerda, campo.margemSuperior - 10);
+            contexto.lineTo(campo.margemDireita, campo.margemSuperior - 10);
+            contexto.setLineDash([5,5]);
+            contexto.strokeStyle = 'white';
+            contexto.stroke();
+
             objetosDaTela.jogador1.desenhar();
             objetosDaTela.jogador2.desenhar();
             objetosDaTela.bolinha.desenhar();
+
+            // this.desenharPlacar();
         },
         atualizar() {
             objetosDaTela.bolinha.atualizar();
+
+            if (objetosDaTela.bolinha.tocouMargemEsquerda()) {
+                if (objetosDaTela.jogador1.defendeuBolinha(objetosDaTela.bolinha.linha)) {
+                    objetosDaTela.bolinha.mudarDirecaoColuna();
+                }
+                else
+                    objetosDaTela.bolinha.pararMovimento();
+            }
+
+            if (objetosDaTela.bolinha.tocouMargemDireita()) {
+                if (objetosDaTela.jogador2.defendeuBolinha(objetosDaTela.bolinha.linha)) {
+                    objetosDaTela.bolinha.mudarDirecaoColuna();
+                }
+                else
+                    objetosDaTela.bolinha.pararMovimento();
+            }
         }
     }
 }
@@ -132,7 +216,6 @@ function loop() {
     telas.jogo.atualizar();
 
     quadro = quadro + 1;
-    console.log(quadro);
     requestAnimationFrame(loop);
 }
 
